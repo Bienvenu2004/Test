5000905e0d2a9a8040882f4f08795f2310c7d7e8
Ambassa Bienvenu <kidsglory1@gmail.com>
2024-12-02 15:34:43 +0100

    added addition and division

--- src/main/java/com/Practice/Main.java
+++ src/main/java/com/Practice/Main.java
@@ -1,11 +1,18 @@
 package com.Practice;
+import com.Practice.ScannerPractices.Addition;
+import com.Practice.ScannerPractices.Division;
 import com.Practice.ScannerPractices.Multiplication;
+
 
 public class Main {
     public static void main(String[] args) {
 
     Multiplication multiplication = new Multiplication();
+        Addition addition = new Addition();
+        Division division = new Division();
     multiplication.TimesTable();
+        addition.TimesTable();
+        division.TimesTable();
 
 
     }
