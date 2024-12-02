e2cdc56222a988e70892e3200c4666e1d9ebf79a
Bienvenu <kidsglory1@gmail.com>
2024-12-02 14:31:49 +0100

    Initial project version

---
+++ src/main/java/com/Practice/Main.java
@@ -0,0 +1,13 @@
+package com.Practice;
+import com.Practice.ScannerPractices.Hcalculator;
+import com.Practice.ScannerPractices.Multiplication;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Multiplication multiplication = new Multiplication();
+    multiplication.TimesTable();
+
+
+    }
+}
---
+++ src/main/java/com/Practice/ScannerPractices/Multiplication.java
@@ -0,0 +1,14 @@
+package com.Practice.ScannerPractices;
+
+public class Multiplication {
+
+    public void TimesTable() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Enter number: ");
+        int number = scanner.nextInt();
+        int i;
+        for (i = 1; i <= 12; i++) {
+            System.out.println(number + " * " + i + " = " + number * i);
+        }
+    }
+}
